e81a90ca1ea459b22b141e47fd684fd4a0e0a396
Juan Vacca <clanbs11.12@gmail.com>
2025-11-22 16:18:02 -0500

    initial commit

---
+++ src/main/java/com/devsenior/svacca/proyecto_integrador_m5/ProyectoIntegradorM5Application.java
@@ -0,0 +1,13 @@
+package com.devsenior.svacca.proyecto_integrador_m5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIntegradorM5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIntegradorM5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/svacca/proyecto_integrador_m5/ProyectoIntegradorM5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.svacca.proyecto_integrador_m5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIntegradorM5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
